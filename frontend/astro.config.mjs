f683d61daa575c2e0e5efcb78b43e60a22cfa947
arc360alt <voidyt16@gmail.com>
2026-04-15 15:54:08 -0500

    Fix a bunch of wierd issues with the API

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
 import favicons from "astro-favicons";
+import node from "@astrojs/node";
 
 export default defineConfig({
+  output: "server",
+  adapter: node({ mode: "standalone" }),
   integrations: [favicons()],
 });
