7ab443b707dc733d6feeb8369b9b49c470b035bf
arc360alt <voidyt16@gmail.com>
2026-04-15 16:05:48 -0500

    dfhghfgdfghdshgfsg

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
 import favicons from "astro-favicons";
-import node from "@astrojs/node";
 
 export default defineConfig({
-  adapter: node({ mode: "standalone" }),
   integrations: [favicons()],
 });
