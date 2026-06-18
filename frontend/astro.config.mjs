2aaa49dbc3b1c13c7f7a96b91d7bfb05228e7c27
arc360alt <voidyt16@gmail.com>
2026-06-18 04:00:54 -0500

    Make this NOT static, we HATE static pages

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
 import favicons from "astro-favicons";
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
+  output: "server",
+  adapter: vercel(),
   integrations: [favicons()],
 });
